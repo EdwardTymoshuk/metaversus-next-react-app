1a99933f1defa7e29cb35d424da70077ed789bb3
Eduard Tymoshuk <edward_t@mbp-edward.home>
2023-01-21 07:57:53 +0100

    remove out folder from .gitignore

---
+++ out/_next/static/JPam6Phf5ElHQ6O6TAWjg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/731-26ae18f525c8718b.js","static/chunks/pages/index-5c57279b32767361.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
